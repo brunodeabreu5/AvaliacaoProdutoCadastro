0b4b5c5ac4601d803c8be77067669172f6252f48
br <brunodornelesdeabreu@hotmail.com>
2021-12-11 13:47:16 -0300

    banckend

---
+++ EstoqueBackend/src/main/java/com/estoque/produtobackend/ProdutoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estoque.produtobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutoBackendApplication.class, args);
+    }
+
+}
